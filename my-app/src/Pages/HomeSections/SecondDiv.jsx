78d10ade1d125a92853ee87c4f0d45d2e3f61443
yash <yash10072000@gmail.com>
2022-10-01 12:02:17 +0530

    added file

---
+++ my-app/src/Pages/HomeSections/SecondDiv.jsx
@@ -0,0 +1,100 @@
+function SecondDiv(){
+    return <>
+    <div style={{borderBottom:".1rem solid #DCDEDD"}}>
+        <div style={{paddingLeft:"60px",paddingRight:"60px"}}>
+            <div style={{display:"flex",paddingBottom:"6rem",paddingLeft:"3.8rem",paddingTop:"2rem",paddingRight:"3.8rem"}}>
+                <div style={{paddingRight:"6rem",paddingTop:"3rem",flex:"1",borderRight:".1rem solid #DCDEDD"}}>
+                    <h3 style={{marginBottom:"1.8rem",color:"#65696",}}>FEATURED PROJECT</h3>
+                    <div>
+                        <div>
+                            <img style={{width:"100%"}} src="https://ksr-ugc.imgix.net/assets/038/425/664/61385629a60211b6d000a17580f1669b_original.jpg?ixlib=rb-4.0.2&crop=faces&w=1024&h=576&fit=crop&v=1661806870&auto=format&frame=1&q=92&s=068aeebd85853dbbedd42215b128945d" alt="" />
+                            <div style={{height:"1.2rem",backgroundColor:"#037362" }}></div>                        
+                        </div>
+                        <h3 style={{paddingBottom:"1.2rem",paddingTop:"1.2rem",fontSize:"2rem",fontWeight:"400"}}>Cupid's Arrows: Volume 2</h3>
+                        <p style={{fontSize:"1.3rem",paddingBottom:"2rem"}}>A collected print edition of the second of Ringo-nominated Thom Zahler's hit webtoon</p>
+                        <p>By Thomas Zahler</p>
+                    </div>
+                </div>
+                <div style={{flex:"1",paddingLeft:"6rem",paddingTop:"3rem"}}>
+                    <h3 style={{marginBottom:"1.8rem"}}>RECOMMENDED FOR YOU</h3>
+                    <div>
+                        <ul style={{listStyle:"none"}}>
+                            <li style={{paddingBottom:"1.8rem",borderBottom:".1rem solid #DCDEDD"}}>
+                                <div style={{display:"flex"}}>
+                                    <div style={{height:"9rem",flexBasis:"40%",paddingRight:"2.4rem"}}>
+                                        <img style={{width:"100%"}} src="https://ksr-ugc.imgix.net/assets/038/628/452/c624c84d0a2c8658bcf5dbee43457e5d_original.png?ixlib=rb-4.0.2&crop=faces&w=560&h=315&fit=crop&v=1663531079&auto=format&frame=1&q=92&s=b0d13471e4f72eaade554f83869759ef" alt="" />
+                                    </div>
+                                    <div>
+                                        <p>The Complete GIANT DAYS Library (for...</p>
+                                        <span style={{fontWeight:"500",fontSize:"1rem",color:"#037362"}}>1,211% funded</span>
+                                        <p style={{fontSize:".8rem",marginTop:"6px"}}>By BOOM! Studios</p>
+                                        <div style={{marginTop:"9px",display:"flex",gap:"10px"}}>
+                                            <button style={{height:"28px",width:"28px",cursor:"pointer",border:".1rem solid #DCDEDD",borderRadius:"100%",justifyContent:"center",alignItems:"center"}}>
+                                                <img style={{width:"1rem",height:"1rem",marginLeft:"4.5px"}} src="https://cdn-icons-png.flaticon.com/512/3106/3106777.png" alt="" />
+                                            </button>
+                                            <button style={{height:"28px",width:"28px",cursor:"pointer",border:".1rem solid #DCDEDD",borderRadius:"100%",justifyContent:"center",alignItems:"center"}}>
+                                                <img style={{width:"1rem",height:"1rem",marginLeft:"4.5px"}} src="https://cdn-icons-png.flaticon.com/512/126/126473.png" alt="" />
+                                            </button>
+                                            <button style={{height:"28px",width:"28px",cursor:"pointer",border:".1rem solid #DCDEDD",borderRadius:"100%",justifyContent:"center",alignItems:"center"}}>
+                                                <img style={{width:"1rem",height:"1rem",marginLeft:"4.5px"}} src="https://cdn-icons-png.flaticon.com/512/126/126504.png" alt="" />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li style={{paddingBottom:"1.8rem",borderBottom:".1rem solid #DCDEDD",paddingTop:"1.8rem"}}>
+                                <div style={{display:"flex"}}>
+                                    <div style={{height:"9rem",flexBasis:"40%",paddingRight:"2.4rem"}}>
+                                        <img style={{width:"100%"}} src="https://ksr-ugc.imgix.net/assets/038/562/594/0cf79820934f9d08db6e88de2003ddff_original.JPEG?ixlib=rb-4.0.2&crop=faces&w=560&h=315&fit=crop&v=1662985015&auto=format&frame=1&q=92&s=d1ff2dfbf7fa47ac8a4b5f15a25c450a" alt="" />
+                                    </div>
+                                    <div>
+                                        <p>Tarot of the Everlasting Day Beginner Deck</p>
+                                        <span style={{fontWeight:"500",fontSize:"1rem",color:"#037362"}}>683% funded</span>
+                                        <p style={{fontSize:".8rem",marginTop:"6px"}}>By Marcus Katz & Tali Goodwin</p>
+                                        <div style={{marginTop:"9px",display:"flex",gap:"10px"}}>
+                                            <button style={{height:"28px",width:"28px",cursor:"pointer",border:".1rem solid #DCDEDD",borderRadius:"100%",justifyContent:"center",alignItems:"center"}}>
+                                                <img style={{width:"1rem",height:"1rem",marginLeft:"4.5px"}} src="https://cdn-icons-png.flaticon.com/512/3106/3106777.png" alt="" />
+                                            </button>
+                                            <button style={{height:"28px",width:"28px",cursor:"pointer",border:".1rem solid #DCDEDD",borderRadius:"100%",justifyContent:"center",alignItems:"center"}}>
+                                                <img style={{width:"1rem",height:"1rem",marginLeft:"4.5px"}} src="https://cdn-icons-png.flaticon.com/512/126/126473.png" alt="" />
+                                            </button>
+                                            <button style={{height:"28px",width:"28px",cursor:"pointer",border:".1rem solid #DCDEDD",borderRadius:"100%",justifyContent:"center",alignItems:"center"}}>
+                                                <img style={{width:"1rem",height:"1rem",marginLeft:"4.5px"}} src="https://cdn-icons-png.flaticon.com/512/126/126504.png" alt="" />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li style={{paddingTop:"1.8rem",paddingBottom:"1.8rem",borderBottom:".1rem solid #DCDEDD"}}>
+                                <div style={{display:"flex"}}>
+                                    <div style={{height:"9rem",flexBasis:"40%",paddingRight:"2.4rem"}}>
+                                        <img style={{width:"100%"}} src="https://ksr-ugc.imgix.net/assets/038/390/830/a6418d462a63736201732a8b2359a9a2_original.jpg?ixlib=rb-4.0.2&crop=faces&w=560&h=315&fit=crop&v=1661450170&auto=format&frame=1&q=92&s=0247a1dc6b44277a5a978a51e3ceaaff" alt="" />
+                                    </div>
+                                    <div>
+                                        <p>The Planetary Academy</p>
+                                        <span style={{fontWeight:"500",fontSize:"1rem",color:"#037362"}}>230% funded</span>
+                                        <p style={{fontSize:".8rem",marginTop:"6px"}}>By Bill Nye, CEO, The Planetary Society</p>
+                                        <div style={{marginTop:"9px",display:"flex",gap:"10px"}}>
+                                            <button style={{height:"28px",width:"28px",cursor:"pointer",border:".1rem solid #DCDEDD",borderRadius:"100%",justifyContent:"center",alignItems:"center"}}>
+                                                <img style={{width:"1rem",height:"1rem",marginLeft:"4.5px"}} src="https://cdn-icons-png.flaticon.com/512/3106/3106777.png" alt="" />
+                                            </button>
+                                            <button style={{height:"28px",width:"28px",cursor:"pointer",border:".1rem solid #DCDEDD",borderRadius:"100%",justifyContent:"center",alignItems:"center"}}>
+                                                <img style={{width:"1rem",height:"1rem",marginLeft:"4.5px"}} src="https://cdn-icons-png.flaticon.com/512/126/126473.png" alt="" />
+                                            </button>
+                                            <button style={{height:"28px",width:"28px",cursor:"pointer",border:".1rem solid #DCDEDD",borderRadius:"100%",justifyContent:"center",alignItems:"center"}}>
+                                                <img style={{width:"1rem",height:"1rem",marginLeft:"4.5px"}} src="https://cdn-icons-png.flaticon.com/512/126/126504.png" alt="" />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+}
+
+export default SecondDiv;
---
+++ my-app/src/Pages/HomeSections/ThirdDiv.jsx
@@ -0,0 +1,20 @@
+function ThirdDiv(){
+    return <>
+    <div>
+        <div style={{paddingTop:"4.8rem",paddingBottom:"4.8rem",paddingLeft:"60px",paddingRight:"60px",borderBottom:".1rem solid #DCDEDD"}}>
+            <div style={{display:"flex",flexBasis:"100%",paddingLeft:"3.8rem",paddingRight:"3.8rem"}}>
+                <div style={{flexBasis:"50%"}}>
+                    <img style={{height:"18rem"}} src="https://ksr-static.imgix.net/ksr-LSS-741_x_240-CENTERED-MODULE-75b63f5.jpg?ixlib=rb-4.0.2&s=8bf40321026cb4b1e0bb52bbf40d1d5a" alt="" />
+                </div>
+                <div style={{paddingLeft:"2.4rem",flexBasis:"50%"}}>
+                    <div style={{borderStyle:"solid",borderLeftWidth:"6px",paddingLeft:"1.8rem",borderColor:"#037362"}}>
+                        <h3 style={{fontSize:"2.8rem",lineHeight:"3.6rem"}}>A Spotlight on short films</h3>
+                        <p style={{paddingTop:"1.8rem",fontSize:"1.5rem",lineHeight:"2rem",fontFamily:""}}>In March 2023, we’re launching Long Story Short, our annual celebration of bold and brilliant short films—and the visionary people who make them.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+}
+export default ThirdDiv;
